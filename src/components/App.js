d259b4f08e6d9ce9b542069ab5af078d81089578
Cindy Poon Yeung <cndpoon@gmail.com>
2022-01-22 23:10:12 -0500

    remove extraneous

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "../styles/index.scss";
-import Header from "./Header";
 import Main from "./Main";
-import Footer from "./Footer";
 
 const App = () => {
   return (
     <>
-      <Header />
       <Main />
     </>
   )
--- src/components/Hero.js
+++
@@ -1,9 +0,0 @@
-const Hero = () => {
-  return (
-    <section className="hero">
-      <h1 className="h1">Hey there</h1>
-    </section>
-  )
-}
-
-export default Hero
